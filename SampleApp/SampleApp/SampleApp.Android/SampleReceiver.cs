a48273e1d770dcc3abf985482d10b7fc7313afdf
jingliancui <ericcui@qq.com>
2020-04-28 08:55:04 +0800

    add source code

---
+++ SampleApp/SampleApp/SampleApp.Android/SampleReceiver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CN.Jpush.Android.Api;
+using CN.Jpush.Android.Service;
+
+namespace SampleApp.Droid
+{
+    
+    [BroadcastReceiver(Enabled = true, Exported = false)]
+    [Android.App.IntentFilter(new string[]
+    {
+        "cn.jpush.android.intent.RECEIVE_MESSAGE",
+    },
+    Categories=new string[] 
+    {
+        "com.companyname.sampleapp"
+    })]
+    public class SampleReceiver : JPushMessageReceiver
+    {
+    }
+    
+}
---
+++ SampleApp/SampleApp/SampleApp.Android/SampleService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using CN.Jpush.Android.Service;
+
+namespace SampleApp.Droid
+{
+    [Android.App.Service(Process = ":pushcore")]
+    [Android.App.IntentFilter(new string[] { "cn.jiguang.user.service.action" })]
+    public class SampleService:JCommonService
+    {
+    }
+}
